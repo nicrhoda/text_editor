2dcbf51b73821bd1bf0834e48811e933393456e9
nic <nrhoda14@gmail.com>
2023-02-15 19:21:47 -0600

    updated gitignore and added register route logic to service worker

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,20 +18,20 @@ export const putDb = async (id, content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, value: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('saved', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('get requset to the database');
+  console.log('get request to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log('result', result);
+  console.log('result.value', result);
   return result;
 }
 

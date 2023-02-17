1ba799f98cd8576501652442b42adbf291beb803
nic <nrhoda14@gmail.com>
2023-02-16 23:05:45 -0600

    added logic for conditional to fix codemirror error onload

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -26,7 +26,13 @@ export default class {
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      
+      //conditional to avoid codemirror error
+      if(!data.length) {
+        this.editor.setValue(localData || header);
+      } else {
+        this.editor.setValue(data || localData || header);
+      }
     });
 
     this.editor.on('change', () => {
